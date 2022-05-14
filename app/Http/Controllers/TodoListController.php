8315eaae18f3b6a862aa7854b21d4c32dc8b66a7
devnafiz <nafiz.2011@yahoo.com>
2022-05-15 00:13:56 +0600

    add model and test

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TodoList;
 
 class TodoListController extends Controller
 {
   public function index(){
 
-    return response(['lists' =>[]]);
+    $list=TodoList::all();
+
+    return response($list);
   }
 }
--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -5,14 +5,17 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\TodoList;
 
 class TodoListTest extends TestCase
 {
    
+   use RefreshDatabase;
+
     public function test_fetch_todo_list()
     {
 
-      
+      TodoList::create(['name'=>'my list']);
         //dd(route('todo-list.store'));
         $response = $this->getJson(route('todo-list.store'));
         //response not json data so add json()
